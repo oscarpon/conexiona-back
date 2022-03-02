9f754eaddf2e7e61fef9bdb905bdbb138ed7482d
Oscar Ponte Baquero <oscar.pontebaquero@gmail.com>
2022-03-02 08:23:59 +0100

    Allowed origins corrected (#11)
    * Aplication properties
    
    * Aplication properties
    
    * Aplication properties
    
    * Allowed origins

--- src/main/java/com/opbaquero/conexionaback/controllers/ReplacementRestController.java
+++ src/main/java/com/opbaquero/conexionaback/controllers/ReplacementRestController.java
@@ -23,7 +23,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.*;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"http://localhost:4200", "https://kanbansense-app.azurewebsites.net/"})
 @RestController
 @RequestMapping("/replacement")
 public class ReplacementRestController {
